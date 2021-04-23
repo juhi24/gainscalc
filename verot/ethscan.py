6381d2b8576c568529390cb1ad295a197956e944
Jussi Tiira <jussi@j24.fi>
2021-04-23 20:21:33 +0300

    package

--- verot/ethscan.py
+++ verot/ethscan.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 import os
+
+import click
 import pandas as pd
 
 
@@ -23,11 +25,20 @@ def load(year, **kws):
     return pd.read_pickle(filepath)
 
 
-if __name__ == "__main__":
+@click.command()
+@click.argument('infile')
+@click.argument('outfile')
+def main(infile, outfile):
+    os.makedirs(os.path.dirname(outfile), exist_ok=True)
     data = load(2020)
     data = data.apply(lambda row: (row.Open+row.Close)/2, axis=1)
-    trades = pd.read_csv('/tmp/export-address-token-0x7.csv', index_col='Txhash', 
+    trades = pd.read_csv(infile, index_col='Txhash', 
                          usecols=('Txhash', 'DateTime', 'Value'), parse_dates=['DateTime'])
     trades['ETHEUR'] = trades.apply(lambda t: data.iloc[data.index.get_loc(t.DateTime, method='pad')], axis=1)
     trades['EURValue'] = trades.apply(lambda row: row.Value*row.ETHEUR, axis=1)
-    trades.to_csv('/tmp/2020eur.csv')
+    trades.to_csv(outfile)
+    
+
+
+if __name__ == "__main__":
+    main()
