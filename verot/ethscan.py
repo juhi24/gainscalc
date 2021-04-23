59a27e70480934c2a3207e4c4feac9f4e5fa54d6
Jussi Tiira <jussi@j24.fi>
2021-04-23 20:25:47 +0300

    dynamic year

--- verot/ethscan.py
+++ verot/ethscan.py
@@ -30,10 +30,11 @@ def load(year, **kws):
 @click.argument('outfile')
 def main(infile, outfile):
     os.makedirs(os.path.dirname(outfile), exist_ok=True)
-    data = load(2020)
-    data = data.apply(lambda row: (row.Open+row.Close)/2, axis=1)
     trades = pd.read_csv(infile, index_col='Txhash', 
                          usecols=('Txhash', 'DateTime', 'Value'), parse_dates=['DateTime'])
+    year = pd.Timestamp(trades.DateTime.values[0]).year
+    data = load(year)
+    data = data.apply(lambda row: (row.Open+row.Close)/2, axis=1)
     trades['ETHEUR'] = trades.apply(lambda t: data.iloc[data.index.get_loc(t.DateTime, method='pad')], axis=1)
     trades['EURValue'] = trades.apply(lambda row: row.Value*row.ETHEUR, axis=1)
     trades.to_csv(outfile)
